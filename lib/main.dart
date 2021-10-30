5fc7722117decd21f18b2f7ed94833dd439d2244
TTC <banglajoy701@gmail.com>
2021-10-30 11:39:10 +0600

    30/10/21

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
+double conheight=100;
+double conwidth=100;
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -32,6 +33,35 @@ class _MyHomePageState extends State<MyHomePage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
+            InkWell(
+             onTap: (){
+        setState(() {
+          conheight=300;
+          conwidth=300;
+        });
+      },
+              onDoubleTap: (){
+                setState(() {
+                  conheight=100;
+                  conwidth=100;
+                });
+              },
+              child: AnimatedContainer(
+                height: conheight,
+                width: conwidth,
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent,
+                    image: DecorationImage(
+                      image: NetworkImage("https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510__480.jpg",
+                      ),
+                      fit: BoxFit.cover
+                    ),
+                    borderRadius: BorderRadius.circular(170),
+                  border: Border.all(color: Colors.black,width: 4)
+                ),
+                duration: Duration(milliseconds: 4000),
+              ),
+            ),
             Image.asset("assets/image/image2.jpeg"),
             Image.asset("assets/image/png.png"),
             Image.asset("assets/image/image3.jpeg"),
